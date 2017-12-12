2ae118fc3beca3121c191a8f6e8068924ba87959
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-12-11 16:04:29 -0800

    update run.py

--- run.py
+++ run.py
@@ -3,16 +3,20 @@ from flask import Flask, Response, abort, request
 import atexit
 from os import environ as env
 import sys
+from dotenv import load_dotenv
 
 app = Flask(__name__, static_url_path='')
 app.config['PROPAGATE_EXCEPTIONS'] = True
-logging.basicConfig(level=logging.FATAL)
 port = os.getenv('VCAP_APP_PORT', '5000')
 
-f = open("/app/metadata.yml", "r")
-raw = f.read()
-original = yaml.load(raw)
-jsonData = json.loads(json.dumps(original))
+@staticmethod
+def get_vcap_credentials(vcap_env, service):
+    if service in vcap_env:
+        vcap_conversation = vcap_env[service]
+        if isinstance(vcap_conversation, list):
+            first = vcap_conversation[0]
+            if 'credentials' in first:
+                return first['credentials']
 
 @atexit.register
 def shutdown():
@@ -21,8 +25,7 @@ def shutdown():
 
 @app.route('/sort', methods=['POST'])
 def sort():
-		print('received image')
-	pass
+	print('received image')
 
 @app.route('/')
 def default():
@@ -34,4 +37,9 @@ def default():
 	
 
 if __name__ == "__main__":
+    vcap_services = os.environ.get("VCAP_SERVICES")
+    if vcap_services:
+        vcap_env = json.loads(vcap_services)
+    if vcap_env:
+    	visual_creds = WatsonEnv.get_vcap_credentials(vcap_env, 'watson_vision_combined')
 	app.run(host='0.0.0.0', port=int(port))
