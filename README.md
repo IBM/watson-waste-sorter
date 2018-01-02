aeff7501ab68d0dd0aad350221693316e792868d
Tommy Li <Tommy.chaoping.li@ibm.com>
2018-01-02 09:51:57 -0800

    Initial Backend Components (#2)
    * add simple server
    
    * update run.py
    
    * Now able to parse visual recognition credentials
    
    * sample post request that takes a sample image to classifier on the default classifier
    
    * update api usage on readme
    
    * update run.py to use the library function to capture the apikey

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # watson-waste-sorter
 ***Work in progress***
+
+# To Use
+
+First provision a Free tier [Visual Recognition](https://console.bluemix.net/catalog/services/visual-recognition) 
+Service and name it `visual-recognition-wws`.
+
+## On Cloud Foundry
+```
+cf push
+```
+
+## Running on Local
+```
+python run.py
+```
+
+## Backend API usage
+
+Do a POST request at `https://watson-waste-sorter.mybluemix.net/api/sort` with the image as the parameter. 
+Return value should be in JSON.
+
+Example in Bash:
+
+Input: png/jpg/jpeg file
+```
+curl -X POST -F "images_file=@fruitbowl.jpg" "https://watson-waste-sorter.mybluemix.net/api/sort"
+```
+
+Output: 
+```
+{
+status code: 200,
+result: landfill,
+accuracy rate: 90
+}
+```
---
+++ run.py
@@ -0,0 +1,40 @@
+import json, requests, os
+from flask import Flask, Response, abort, request
+from os.path import join, dirname
+from watson_developer_cloud import VisualRecognitionV3
+from watson_developer_cloud import watson_service
+
+app = Flask(__name__, static_url_path='')
+app.config['PROPAGATE_EXCEPTIONS'] = True
+port = os.getenv('VCAP_APP_PORT', '5000')
+
+# Global variables for credentials
+url = ''
+apikey = ''
+
+# API destination
+@app.route('/api/sort', methods=['POST'])
+def sort():
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key=apikey)
+    sampleImageUrl = 'https://www.ibm.com/ibm/ginni/images/ginni_bio_780x981_v4_03162016.jpg'
+    #sample parameter json
+    #{ "classifier_ids": ["watson-waste-sorter-classifier-id"]}
+    url_result = visual_recognition.classify(parameters=json.dumps({'url': sampleImageUrl}))
+    print(json.dumps(url_result, indent=2))
+    return ''
+
+# Default frontend page.
+@app.route('/')
+def default():
+    try:
+        print(request.headers)
+    except:
+        pass
+    return ''
+    
+
+if __name__ == "__main__":
+    visual_creds = watson_service.load_from_vcap_services('watson_vision_combined')
+    url = visual_creds['url']
+    apikey = visual_creds['api_key']
+    app.run(host='0.0.0.0', port=int(port))
