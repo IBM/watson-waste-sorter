4caeb4d4896d62dde3c153acd1567d9c6849a684
Tommy Li <Tommy.chaoping.li@ibm.com>
2018-01-10 11:55:12 -0800

    add CI, Track, and IBM Cloud button

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-import json, requests, os
+import json
+import os
 import logging
-from flask import Flask, Response, abort, request
-from os.path import join, dirname
+from flask import Flask, request
 from watson_developer_cloud import VisualRecognitionV3
 from watson_developer_cloud import watson_service
+import metrics_tracker_client
 
 app = Flask(__name__, static_url_path='')
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -14,14 +15,15 @@ port = os.getenv('VCAP_APP_PORT', '5000')
 url = ''
 apikey = ''
 
+
 # API destination
 @app.route('/api/sort', methods=['POST'])
 def sort():
     try:
         images_file = request.files.get('images_file', '')
         visual_recognition = VisualRecognitionV3('2016-05-20', api_key=apikey)
-        #sample parameter json
-        #{ "classifier_ids": ["watson-waste-sorter-classifier-id"]}
+        # sample parameter json
+        # { "classifier_ids": ["watson-waste-sorter-classifier-id"]}
         url_result = visual_recognition.classify(images_file=images_file)
         list_of_result = url_result["images"][0]["classifiers"][0]["classes"]
         result_class = ''
@@ -30,18 +32,24 @@ def sort():
             if result["score"] >= result_score:
                 result_score = result["score"]
                 result_class = result["class"]
-        return json.dumps({"status code": 200, "result" : result_class, "accuracy rate": result_score})
-    except Exception as err:
-        return json.dumps({"status code": 500, "result" : "Not an image", "accuracy rate": 0})
+        return json.dumps(
+            {"status code": 200, "result": result_class,
+                "accuracy rate": result_score})
+    except Exception:
+        return json.dumps(
+            {"status code": 500, "result": "Not an image", "accuracy rate": 0})
+
 
 # Default frontend page.
 @app.route('/')
 def default():
     return ''
-    
+
 
 if __name__ == "__main__":
-    visual_creds = watson_service.load_from_vcap_services('watson_vision_combined')
+    visual_creds = watson_service.load_from_vcap_services(
+        'watson_vision_combined')
     url = visual_creds['url']
     apikey = visual_creds['api_key']
+    metrics_tracker_client.track()
     app.run(host='0.0.0.0', port=int(port))
