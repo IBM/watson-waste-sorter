2da99c4441bff913d64fc5f29d72f90f895019be
Tommy Li <Tommy.chaoping.li@ibm.com>
2018-02-13 14:51:52 -0800

    update ios app

--- mobile-app/Pods/Target Support Files/Pods-WatsonWasteSorter/Pods-WatsonWasteSorter-frameworks.sh
+++ mobile-app/Pods/Target Support Files/Pods-WatsonWasteSorter/Pods-WatsonWasteSorter-frameworks.sh
@@ -136,11 +136,13 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CameraManager/CameraManager.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SimpleAlert/SimpleAlert.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CameraManager/CameraManager.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SimpleAlert/SimpleAlert.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
